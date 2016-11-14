2323cc511715d18331cc30a426c904d9bb66be9b
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-11-14 10:37:19 -0600

    Renamed activity

--- DrakeActivities/app/src/main/java/cs188/drakeactivities/MainActivity.java
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/MainActivity.java
@@ -3,7 +3,7 @@ package cs188.drakeactivities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class TodayView extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
