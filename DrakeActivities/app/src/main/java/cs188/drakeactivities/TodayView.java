a34a3eadbbdd8cdb674cbc3d80e6cc7259903f16
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-11-14 10:23:21 -0600

    Init Commit
    Added initial files

---
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/TodayView.java
@@ -0,0 +1,13 @@
+package cs188.drakeactivities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TodayView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_today_view);
+    }
+}
