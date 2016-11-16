449c7ad3c2abd19c00c33e31a48b08203a3bc23c
ColeSpletstoser <cole.spletstoser@drake.edu>
2016-11-15 18:45:47 -0600

    ViewPager
    Set up view pager so you can swipe between fragments. Still need to
    make tabs clickable

---
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/CalendarFragment.java
@@ -0,0 +1,23 @@
+package cs188.drakeactivities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Spletz on 11/15/16.
+ */
+public class CalendarFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View layout = inflater.inflate(R.layout.calendar_fragment, container, false);
+
+        //do work here
+
+        return layout;
+    }
+}
--- DrakeActivities/app/src/main/java/cs188/drakeactivities/MainActivity.java
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/MainActivity.java
@@ -1,13 +1,101 @@
 package cs188.drakeactivities;
 
+import android.content.Context;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_today_view);
+        setContentView(R.layout.activity_main);
+
+        CustomAdapter adapter = new CustomAdapter(getSupportFragmentManager(), getApplicationContext());
+
+        viewPager = (ViewPager) findViewById(R.id.viewPager);
+        viewPager.setAdapter(adapter);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
+
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+
+        viewPager.setCurrentItem(0, false);
+
+        tabLayout.setTabsFromPagerAdapter(adapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    private class CustomAdapter extends FragmentPagerAdapter{
+        public CustomAdapter(FragmentManager supportFragmentManager, Context applicationContext) {
+            super(supportFragmentManager);
+        }
+
+        public boolean userHasAccount = true;
+
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    return new TodayFragment();
+                case 1:
+                    return new CalendarFragment();
+                case 2:
+                    if(userHasAccount) //change to check for if the user has an account
+                        return new ProfileFragment();
+                    else
+                        return new LogInFragment();
+                default:
+                    return null;
+            }
+        }
+
+        @Override
+        public int getCount() {
+            return 3;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position) {
+            String title=" ";
+            switch (position){
+                case 0:
+                    title="Today";
+                    break;
+                case 1:
+                    title="Calendar";
+                    break;
+                case 2:
+                    title="Profile";
+                    break;
+            }
+
+            return title;
+        }
     }
 }
