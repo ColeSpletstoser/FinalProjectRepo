52c7d89b60d6c3f83b02bac06fc7459ad6965715
tommyrauch1 <thomas.rauch@drake.edu>
2016-11-18 09:42:49 -0600

    Floating action button
    added FAB, changed layout name to be correct

--- DrakeActivities/app/src/main/java/cs188/drakeactivities/EventDescription.java
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/EventDescription.java
@@ -4,20 +4,22 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
 
 
 public class EventDescription extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.today_fragment, container, false);
+        View layout = inflater.inflate(R.layout.fragment_event_description, container, false);
+        View v =  inflater.inflate(R.layout.fragment_event_description, container, false);
 
-        
+        FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
 
         return layout;
     }
