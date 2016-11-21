182f456d8c286d1321b706be702e4518a3f3ef80
LuxColtin <coltin.lux@drake.edu>
2016-11-21 10:27:43 -0600

    Deleted TextView in Calendar Frag

--- DrakeActivities/app/src/main/java/cs188/drakeactivities/CalendarFragment.java
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/CalendarFragment.java
@@ -6,7 +6,6 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by Spletz on 11/15/16.
@@ -18,8 +17,6 @@ public class CalendarFragment extends Fragment {
         View layout = inflater.inflate(R.layout.calendar_fragment, container, false);
 
         //do work here
-
-        TextView txtView = (TextView) layout.findViewById(R.id.calendarfrag);
 
         return layout;
     }
