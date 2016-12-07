e630572c83d76996727a784dc3f4f6a3c43fa6b6
tommyrauch1 <thomas.rauch@drake.edu>
2016-12-07 10:16:11 -0600

    Toasts and stuff

--- DrakeActivities/app/src/main/java/cs188/drakeactivities/EventDescription.java
+++ DrakeActivities/app/src/main/java/cs188/drakeactivities/EventDescription.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
@@ -11,26 +12,49 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 
 public class EventDescription extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View layout = inflater.inflate(R.layout.fragment_event_description, container, false);
         View v =  inflater.inflate(R.layout.fragment_event_description, container, false);
+
+        Button enter = (Button) v.findViewById(R.id.enterCode);
+
+        final EditText code = (EditText) v.findViewById(R.id.eventCode);
 
         FloatingActionButton fab = (FloatingActionButton) v.findViewById(R.id.fab);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //TODO: have button bring up three other buttons (ask clayton), save to phone, app, or both calendars
-                //Intent in = new Intent(getActivity(), .class);
-                // startActivity(in);
+                int duration = Toast.LENGTH_SHORT;
+                Context context = getActivity();
+                Toast toast = Toast.makeText(context, "Saved event to calendar", duration);
+                toast.show();
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setData(CalendarContract.Events.CONTENT_URI);
+
+                startActivity(calIntent);
+
             }
         });
 
-        return layout;
+        enter.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                int duration = Toast.LENGTH_SHORT;
+                Context context = getActivity();
+                Toast toast = Toast.makeText(context, "Points awarded", duration);
+                toast.show();
+            }
+        });
+
+        return v;
     }
 }
+
+
